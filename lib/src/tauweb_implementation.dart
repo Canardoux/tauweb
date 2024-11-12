c849ba336915dede10ca05445d8405a167b9a6c1
larpoux <larpoux@gmail.com>
2024-11-12 19:42:29 +0100

    Assets OK

--- lib/src/tauweb_implementation.dart
+++ lib/src/tauweb_implementation.dart
@@ -53,7 +53,7 @@ class TauwebImplementation implements TauInterface {
     _logger = log.Logger();
     log.Logger.level = loglevel;
     if (!alreadyInited) {
-      await importModule("./packages/tau_web/js/tau_web.js".toJS).toDart;
+      await importModule("./assets/packages/tau_web/assets/js/tau_web.js".toJS).toDart;
       alreadyInited = true;
     }
   }
